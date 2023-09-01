f5535dab320604d30d9a85f5c1fe827f06790c9f
fuma <101544710+fumaSuzuki@users.noreply.github.com>
2023-09-01 16:23:59 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+//スクロールした際の動きを関数でまとめる
+function PageTopAnime() {
+	var scroll = $(window).scrollTop();
+	if (scroll >= 200){//上から200pxスクロールしたら
+		$('#page-top').removeClass('DownMove');//#page-topについているDownMoveというクラス名を除く
+		$('#page-top').addClass('UpMove');//#page-topについているUpMoveというクラス名を付与
+	}else{
+		if($('#page-top').hasClass('UpMove')){//すでに#page-topにUpMoveというクラス名がついていたら
+			$('#page-top').removeClass('UpMove');//UpMoveというクラス名を除き
+			$('#page-top').addClass('DownMove');//DownMoveというクラス名を#page-topに付与
+		}
+	}
+}
+
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function () {
+	PageTopAnime();/* スクロールした際の動きの関数を呼ぶ*/
+});
+
+// ページが読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load', function () {
+	PageTopAnime();/* スクロールした際の動きの関数を呼ぶ*/
+});
+
+// #page-topをクリックした際の設定
+$('#page-top a').click(function () {
+    $('body,html').animate({
+        scrollTop: 0//ページトップまでスクロール
+    }, 100);//ページトップスクロールの速さ。数字が大きいほど遅くなる
+    return false;//リンク自体の無効化
+});
+
+const targets = document.getElementsByClassName('fade');
+for (let i = targets.length; i--;) {
+    let observer = new IntersectionObserver((entries, observer) => {
+        let j = entries.findIndex((e) => {return e.isIntersecting}); // isIntersectingの要素のインデックスを返す
+        entries[j].target.classList.add('active');
+        observer.unobserve(entries[j].target);
+    });
+    observer.observe(targets[i]);
+}
